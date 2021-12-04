6025a6d537b4f1e9f73f23493abf98a06a629cda
Arpitha Bhat <arpithabht12@gmail.com>
2021-12-04 18:56:57 +0530

    [Arpitha]ADDED: practice code for template literals

---
+++ TemplateLiterals.js
@@ -0,0 +1,10 @@
+let str = `Hello world 
+Trying out multiline string 
+using es6 feature`;
+console.log(str);
+
+const a = 10;
+const b = 25;
+
+const ab = `Age of a is: ${a} and Weight of a is: ${b} and sum of height and weight is : ${a+b}`;
+console.log(ab)
